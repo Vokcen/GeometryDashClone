2d67802fde96aa9aa4c0ebac60970c12458289aa
Vokcen <81093058+Vokcen@users.noreply.github.com>
2023-08-10 19:24:46 +0300

    Some codes cleans.

--- Assets/_Game/01_Scripts/CubeSpawner.cs
+++ Assets/_Game/01_Scripts/CubeSpawner.cs
@@ -24,15 +24,5 @@ public class CubeSpawner : MonoBehaviour
     {
         EventManager.OnHitSpike -= OnHitSpike;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/_Game/01_Scripts/HitDetect.cs
+++ Assets/_Game/01_Scripts/HitDetect.cs
@@ -6,15 +6,11 @@ using UnityEngine;
 public class HitDetect : MonoBehaviour
 {
 
-    private void OnCollisionEnter(Collision collision)
-    {
-      
-    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent<CubeMovement>(out CubeMovement cube))
         {
-            print("hit");
             EventManager.OnHitSpike?.Invoke(cube, EventArgs.Empty);
         }
     }
